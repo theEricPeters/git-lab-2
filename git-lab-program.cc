fe6d064fc86216c27fbef322196171aeebb326f7
theEricPeters <diamondshadow88@gmail.com>
2020-09-04 15:00:28 -0400

    Added git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/**
+ *        @file: git-lab-program.cc
+ *      @author: Eric Peters
+ *        @date: September 04, 2020
+ *       @brief: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+   return 0;
+}// main
